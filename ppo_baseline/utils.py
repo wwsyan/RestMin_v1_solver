16588d7b5dfb4382c60e10d96b2c911d9b6b0eb6
550C fantacy <52427538+wwsyan@users.noreply.github.com>
2023-05-07 18:15:46 +0800

    Update utils.py

--- ppo_baseline/utils.py
+++ ppo_baseline/utils.py
@@ -33,53 +33,6 @@ def grid_trans(np_array, trans_type):
     return deepcopy(new_array)
 
 
-def get_cluster_num(state):
-    # 返回区域的数量: 类扫雷的无雷区展开机制
-    SIZE = state["obs"].shape[0]
-    def get_cluster(x, y):
-        cluster = []
-        check_box = []
-        check_box.append((x, y))
-        while len(check_box):
-            c_x, c_y = check_box.pop() # check index (x, y) from checkbox
-            Gid = base[0, c_x, c_y]
-            base[1, c_x, c_y] = 0
-            cluster.append((c_x, c_y))
-            if c_x - 1 >= 0:
-                if base[0, c_x - 1, c_y] == Gid and base[1, c_x - 1, c_y]:
-                    check_box.append((c_x - 1, c_y))
-            if c_x + 1 < SIZE:
-                if base[0, c_x + 1, c_y] == Gid and base[1, c_x + 1, c_y]:
-                    check_box.append((c_x + 1, c_y))
-            if c_y - 1 >= 0:
-                if base[0, c_x, c_y - 1] == Gid and base[1, c_x, c_y - 1]:
-                    check_box.append((c_x, c_y - 1))
-            if c_y + 1 < SIZE:
-                if base[0, c_x, c_y + 1] == Gid and base[1, c_x, c_y + 1]:
-                    check_box.append((c_x, c_y + 1))
-        return cluster
-    
-    base = np.ones((2, SIZE, SIZE))
-    base[0] = state['obs']
-    clusters = []
-    for i in range(SIZE):
-        for j in range(SIZE):
-            base[1, i, j] = 0
-            if base[0, i, j] == 0:
-                continue
-            if clusters == []:
-                clusters.append(get_cluster(i, j))
-            flag = True
-            for cluster in clusters:
-                if (i, j) in cluster:
-                    flag = False
-                    break
-            if flag:
-                clusters.append(get_cluster(i, j))
-                
-    return len(clusters)
-
-
 
 
 
