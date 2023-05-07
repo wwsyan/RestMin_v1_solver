001c7a40e0e036586231973ac4be473a205c473e
550C fantacy <52427538+wwsyan@users.noreply.github.com>
2023-05-07 18:16:58 +0800

    Update run.py

--- ppo_baseline/run.py
+++ ppo_baseline/run.py
@@ -3,18 +3,17 @@ import torch as th
 
 from sb3_contrib import MaskablePPO
 from sb3_contrib.common.wrappers import ActionMasker
-from callback_da import DataAugmentCallback
 from env import RestMinEnv_v1
 
 def mask_fn(env):
     return env.get_action_mask()
 
 if __name__ == "__main__":
-    env = RestMinEnv_v1(size=6, mode=1, use_cluster_reward=True)
+    env = RestMinEnv_v1(size=6, mode=0)
     env = ActionMasker(env, mask_fn)
     
     policy_kwargs = dict(activation_fn=th.nn.Tanh,
-                          net_arch=dict(pi=[64, 64, 128], vf=[64, 64, 32]))
+                          net_arch=dict(pi=[64, 64], vf=[64, 64]))
     
     try:
         model = MaskablePPO.load(path="ppo", env=env)
@@ -23,7 +22,7 @@ if __name__ == "__main__":
                             env=env, 
                             learning_rate=3e-4,
                             n_steps=2048, 
-                            batch_size=64,
+                            batch_size=512,
                             n_epochs=10,
                             gamma=0.99,
                             gae_lambda=0.95,
@@ -40,9 +39,7 @@ if __name__ == "__main__":
                             verbose=2,
                             )
     
-    DACallback = DataAugmentCallback(env=env, model=model, use_DA=True)
-    
-    model.learn(50e4, callback=DACallback)
+    model.learn(100e4)
     model.save("ppo")
     del model
     
